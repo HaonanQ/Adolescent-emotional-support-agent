912dabd4225b3d4fb965e7e419862c27bb5361a7
Qiuhaonan <2978607578@qq.com>
2026-02-21 23:22:17 +0800

    可以使用在线模型

--- src/main/java/com/qhn/qhnaiagent/controller/HealthController.java
+++ src/main/java/com/qhn/qhnaiagent/controller/HealthController.java
@@ -1,9 +1,10 @@
 package com.qhn.qhnaiagent.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("/health")
 public class HealthController {
--- src/main/java/com/qhn/qhnaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/qhn/qhnaiagent/demo/invoke/TestApiKey.java
@@ -6,5 +6,5 @@ package com.qhn.qhnaiagent.demo.invoke;
 public interface TestApiKey {
 
     // 修改为你的 API Key
-    String API_KEY = "修改为你的 API Key";
+    String API_KEY = "sk-de045a45875646ba9becee1bfac084ba";
 }
